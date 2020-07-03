e188bec318f5f434b326950315befa6e313eec34
Mozuha <100zuha.shikitomi@gmail.com>
2020-07-03 18:04:07 +0900

    connected

---
+++ Backend/api/venv/static/react/precache-manifest.b51dc87214223ba2cd76f4dd322ae75f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9aadefe5fa33f4ebc9d62cb8dc1d592",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "3946b96600a32bf035ea",
+    "url": "/static/react/js/2.d399d8d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/react/js/2.d399d8d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072c19a36d1856079c36",
+    "url": "/static/react/js/main.707448a2.chunk.js"
+  },
+  {
+    "revision": "c374789414c38078e5d8",
+    "url": "/static/react/js/runtime-main.e2c43dbe.js"
+  }
+]);
